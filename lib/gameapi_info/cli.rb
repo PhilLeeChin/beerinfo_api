821af584b8993649c96d21339a3f9fca075ae36d
PhilLeeChin <phil.leechin@outlook.com>
2020-03-25 12:58:43 -0400

    uncomment options_listing instance method

--- lib/gameapi_info/cli.rb
+++ lib/gameapi_info/cli.rb
@@ -1,5 +1,24 @@
 class CLI
-  def test
-    puts "Welcome"
+  def open
+    self.welcome_message
+    self.options_listing
   end
+
+  def welcome_message
+    puts "**************************************************"
+    puts "**************************************************"
+    puts "*****      Welcome to my game info app       *****"
+    sleep(2)
+    puts "*****   Select from 1 of the options below   *****"
+    puts "**************************************************"
+    puts "**************************************************"
+  end
+
+  def options_listing
+    #returns the listing based on the request
+    API.get_listings
+    binding.pry
+  end
+
+
 end

cf0b16c12019cbb2efe29f9f7acbdb6fd054a524
PhilLeeChin <phil.leechin@outlook.com>
2020-03-25 18:43:18 -0400

    add new api url

--- lib/gameapi_info/api.rb
+++ lib/gameapi_info/api.rb
@@ -1,9 +1,8 @@
 class API
   def self.get_listings
-    gia_key = ENV["GAMEINFO_API_KEY"]
-    gia_url = "https://api-v3.igdb.com/games/"
+    #pa_url = "https://api.punkapi.com/v2/beers/"
+    response = HTTParty.get('https://api.punkapi.com/v2/beers')
     binding.pry
-    response = HTTParty.get(gia_url, :headers => {"user-key" => "#{gia_key}"})
     response
   end
 end

95b5dbd595ecdee3b56cec6b4ebd1b349e8de046
PhilLeeChin <phil.leechin@outlook.com>
2020-03-27 17:03:58 -0400

    add beer file

--- config/environment.rb
+++ config/environment.rb
@@ -5,4 +5,5 @@ require 'pry'
 #connects to the api, cli, and version file in the gameapi_info folder
 require_relative "../lib/gameapi_info/api"
 require_relative "../lib/gameapi_info/cli"
+require_relative "../lib/gameapi_info/beer"
 require_relative "../lib/gameapi_info/version"

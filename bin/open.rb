d5f1f8d48411590b7db31e942d65abdfe278f113
PhilLeeChin <phil.leechin@outlook.com>
2020-03-30 15:32:44 -0400

    add comment

--- bin/open.rb
+++ bin/open.rb
@@ -3,4 +3,5 @@
 #connects to the environment file in the config file
 require_relative "../config/environment"
 
+#calls on the CLI class, creating an new instance with the open instance method
 CLI.new.open

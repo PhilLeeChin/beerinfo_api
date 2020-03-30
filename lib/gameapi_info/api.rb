be662779b079fac448b62c265b6e84ad56bbe3be
PhilLeeChin <phil.leechin@outlook.com>
2020-03-30 15:34:56 -0400

    add comment, and new limit for beer count

--- lib/gameapi_info/api.rb
+++ lib/gameapi_info/api.rb
@@ -1,11 +1,11 @@
-# API class created to gather information from the punkapi website.
-# This API is created to provide details on different types of beers
-# For this app it will be pulling the first 10 beers
+# API class gathers information from the punkapi website.
+# This API provides details on different types of beers
+# For this app it will be pulling the first 15 beers
 class API
   def self.get_listings
     #pa_url = "https://api.punkapi.com/v2/beers/"
-    #stores the return data from the API in response
-    response = HTTParty.get('https://api.punkapi.com/v2/beers/?page=1&per_page=10')
+    #stores the return data from the API in response variable
+    response = HTTParty.get('https://api.punkapi.com/v2/beers/?page=1&per_page=15')
     #itterates through the data stored in response for each beer
     response.each do |beer|
       #assigns a new Beer class with the name of the beer, tagline, first_brewed, and the description
